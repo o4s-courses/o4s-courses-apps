64d5a21fcfff0dbbe3f4d54076c1e889b41c13c2
joseantcordeiro <joseantcordeiro@gmail.com>
2023-04-30 13:58:38 +0100

    authOptions

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/email";
 
 import { prisma } from "@o4s/db";
 
@@ -15,7 +15,7 @@ declare module "next-auth" {
     user: {
       id: string;
       // ...other properties
-      // role: UserRole;
+      role: string;
     } & DefaultSession["user"];
   }
 
@@ -31,29 +31,35 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  **/
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
-      return session;
-    },
-  },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
     }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
   ],
+  callbacks: {
+    session({ session, token, user }) {
+      if (session.user) {
+        session.user.id = user.id
+        session.user.role = user.role
+      }
+
+      return session
+    }
+  },
+  theme: {
+    colorScheme: "dark", // "auto" | "dark" | "light"
+    brandColor: "#1FB2A5", // Hex color code
+    logo: "/images/logoO4S-70alt.png", // Absolute URL to image
+    // buttonText: "" // Hex color code
+  }
 };
+
---
+++ packages/config/app/index.ts
@@ -0,0 +1,15 @@
+export const AppConfig = {
+    site_name: 'O4S Courses',
+    title: 'O4S Courses',
+    description: 'Online Courses for Sustainability',
+    locale: 'en',
+  };
+
+export const PublicFooterNavigation = [
+    { name: "Features", href: "/#features" },
+    { name: "Lessons", href: "/tutorials/cs50" },
+    { name: "Pricing", href: "/#pricing" },
+    { name: "Terms of Service", href: "/" },
+    { name: "Privacy Policy", href: "/" },
+    { name: "About us", href: "/" },
+];
