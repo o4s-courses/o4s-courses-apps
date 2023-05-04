1e98993e86c5af11212644f081d85276bc009388
José Cordeiro <joseantcordeiro@gmail.com>
2023-05-04 12:18:36 +0000

    Admin courses app

--- apps/admin/src/components/TableCourses.tsx
+++ apps/admin/src/components/TableCourses.tsx
@@ -1,69 +1,62 @@
+import { api, type RouterOutputs } from "~/utils/api";
 
-type Props = {
-  courses: {
-    id: number;
-		name: string;
-		description: string;
-		published: boolean;
-  }[];
+const CourseCard: React.FC<{
+  course: RouterOutputs["course"]["byAuthor"][number];
+  onCourseDelete?: () => void;
+}> = ({ course, onCourseDelete }) => {
+  return (
+    <div className="flex flex-row rounded-lg bg-white/10 p-4 transition-all hover:scale-[101%]">
+      <div className="flex-grow">
+        <h2 className="text-2xl font-bold text-pink-400">{course.name}</h2>
+        <p className="mt-2 text-sm">{course.description}</p>
+				<p className="mt-2 text-sm">Lessons: {course._count.lessons} | Students: {course._count.students}</p>
+      </div>
+      <div>
+        <span
+          className="cursor-pointer text-sm font-bold uppercase text-pink-400"
+          onClick={onCourseDelete}
+        >
+          Delete
+        </span>
+      </div>
+    </div>
+  );
 };
 
-export const TableCourses = ({ courses }: Props) => {
+const TableCourses = () => {
+	const courseQuery = api.course.byAuthor.useQuery();
+
+	const deleteCourseMutation = api.course.delete.useMutation({
+    onSettled: () => courseQuery.refetch(),
+  });
 
 	return (
-			<div className="max-w-screen-xl mx-auto px-4 md:px-8">
-					<div className="items-start justify-between md:flex">
-							<div className="max-w-lg">
-									<h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
-											Todos os cursos
-									</h3>
-									<p className="text-gray-600 mt-2">
-											Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-									</p>
-							</div>
-							<div className="mt-3 md:mt-0">
-									<a
-											href="javascript:void(0)"
-											className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
-									>
-											Adicionar curso
-									</a>
-							</div>
-					</div>
-					<div className="mt-12 relative h-max overflow-auto">
-							<table className="w-full table-auto text-sm text-left">
-									<thead className="text-gray-600 font-medium border-b">
-											<tr>
-													<th className="py-3 pr-5">ID</th>
-													<th className="py-3 pr-5">Nome</th>
-													<th className="py-3 pr-5">Descrição</th>
-													<th className="py-3 pr-5">Estado</th>
-													<th className="py-3 pr-5"></th>
-											</tr>
-									</thead>
-									<tbody className="text-gray-600 divide-y">
-											{
-													courses.map((item, idx) => (
-															<tr key={idx}>
-																	<td className="pr-5 py-4 whitespace-nowrap">{item.id}</td>
-																	<td className="pr-5 py-4 whitespace-nowrap">{item.name}</td>
-																	<td className="pr-5 py-4 whitespace-nowrap">
-																			<span className={`px-3 py-2 rounded-full font-semibold text-xs ${item.published ? "text-green-600 bg-green-50" : "text-blue-600 bg-blue-50"}`}>
-																					{item.published ? "S" : "N"}
-																			</span>
-																	</td>
-																	<td className="pr-5 py-4 whitespace-nowrap">{item.description}</td>
-																	<td className="text-right whitespace-nowrap">
-																			<a href="javascript:void()" className="py-1.5 px-3 text-gray-600 hover:text-gray-500 duration-150 hover:bg-gray-50 border rounded-lg">
-																					Gerir
-																			</a>
-																	</td>
-															</tr>
-													))
-											}
-									</tbody>
-							</table>
-					</div>
-			</div>
+		<>
+		{courseQuery.data ? (
+      <div className="w-full">
+        {courseQuery.data?.length === 0 ? (
+          <span>There are no courses!</span>
+        ) : (
+          <div className="flex justify-center overflow-y-scroll px-4 text-2xl">
+            <div className="flex w-full flex-col gap-4">
+              {courseQuery.data?.map((p) => {
+                return (
+                  <CourseCard
+                    key={p.id}
+                    course={p}
+                    onCourseDelete={() => deleteCourseMutation.mutate(p.id)}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+    ) : (
+      <p>Loading...</p>
+    )}
+		</>
 	)
-}
+};
+
+export default TableCourses;
--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import SectionWrapper from "~/components/SectionWrapper";
 import { type GetServerSideProps, type NextPage } from 'next/types';
 import { getServerSession, type Session } from "@o4s/auth";
@@ -7,75 +6,21 @@ import { api, type RouterOutputs } from "~/utils/api";
 import Header from "~/components/ui/Header";
 import Nav from "~/components/ui/Nav";
 import Footer from "~/components/ui/Footer";
+import TableCourses from "~/components/TableCourses";
 
 type Props = {
 	session: Session;
 	isAdmin: boolean;
 };
 
-const CourseCard: React.FC<{
-  course: RouterOutputs["course"]["all"][number];
-  onCourseDelete?: () => void;
-}> = ({ course, onCourseDelete }) => {
-  return (
-    <div className="flex flex-row rounded-lg bg-white/10 p-4 transition-all hover:scale-[101%]">
-      <div className="flex-grow">
-        <h2 className="text-2xl font-bold text-pink-400">{course.name}</h2>
-        <p className="mt-2 text-sm">{course.description}</p>
-				<p className="mt-2 text-sm">Lessons: {course._count.lessons}</p>
-				<p className="mt-2 text-sm">Students: {course._count.students}</p>
-      </div>
-      <div>
-        <span
-          className="cursor-pointer text-sm font-bold uppercase text-pink-400"
-          onClick={onCourseDelete}
-        >
-          Delete
-        </span>
-      </div>
-    </div>
-  );
-};
-
 const Home: NextPage<Props> = ({ isAdmin }) => {
 	const { data: session } = api.auth.getSession.useQuery();
-  const courseQuery = api.course.byAuthor.useQuery();
-
-	const deleteCourseMutation = api.course.delete.useMutation({
-    onSettled: () => courseQuery.refetch(),
-  });
-
 
 	return (
 		<><Header title="Cursos - Admin" />
 			<Nav />
 			<SectionWrapper className="mt-12 dark:mt-0">
-				
-
-					{courseQuery.data ? (
-            <div className="w-full max-w-2xl">
-              {courseQuery.data?.length === 0 ? (
-                <span>There are no courses!</span>
-              ) : (
-                <div className="flex h-[40vh] justify-center overflow-y-scroll px-4 text-2xl">
-                  <div className="flex w-full flex-col gap-4">
-                    {courseQuery.data?.map((p) => {
-                      return (
-                        <CourseCard
-                          key={p.id}
-                          course={p}
-                          onCourseDelete={() => deleteCourseMutation.mutate(p.id)}
-                        />
-                      );
-                    })}
-                  </div>
-                </div>
-              )}
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
-
+				<TableCourses />
 			</SectionWrapper>
 			<Footer />
 		</>
